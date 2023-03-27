29d59e5506af9946876830c292f9e3f9e588f78a
Nathan Derhore <nathan@derhore.fr>
2023-03-27 19:05:30 +0200

    implements docker rabbitmq

--- src/main/java/com/studi/iot/IotApplication.java
+++ src/main/java/com/studi/iot/IotApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class IotApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(IotApplication.class, args);
 	}
 
